f42e8ade4ad943ca82804853b5a002ac73dbfddb
Quentin <quentin.quero22@gmail.com>
2023-01-02 15:31:42 +0100

    pénueries, machineBuilder OK

--- Machine.java
+++ Machine.java
@@ -1,21 +1,33 @@
+package machineacafe;
+
 public class Machine {
-    private int _nombreCafesServis = 0;
+    private int _nombreCafésServis = 0;
+    private double _argentEncaissé = 0;
+    private boolean _eauDisponible = true;
+    private boolean _gobeletsDisponibles = true;
+    private boolean _caféEnStock = true;
 
-    private double _argentEncaisse = 0;
+    public Machine(int nombreGobeletsInitial, int dosesCaféInitiales) {
+        _gobeletsDisponibles = nombreGobeletsInitial > 0; // TODO : contradiction à résoudre avec le client
+        _caféEnStock = dosesCaféInitiales > 0;
+    }
 
-    public void Inserer(double somme){
-        if(somme >= 0.4)
-        {
-            _argentEncaisse += somme;
-            _nombreCafesServis ++;
+    public void Insérer(double somme) {
+        if(somme >= 0.4 && _eauDisponible && _gobeletsDisponibles && _caféEnStock){
+            _argentEncaissé += somme;
+            _nombreCafésServis ++;
         }
     }
 
-    public int GetNombreCafesServis() {
-        return _nombreCafesServis;
+    public int GetNombreCafésServis() {
+        return _nombreCafésServis;
     }
 
-    public double GetArgentEncaisse() {
-        return _argentEncaisse;
+    public double GetArgentEncaissé() {
+        return _argentEncaissé;
+    }
+
+    public void CouperEau() {
+        _eauDisponible = false;
     }
 }
--- test/MachineACafeTest.java
+++ test/MachineACafeTest.java
@@ -1,75 +1,52 @@
-
-
-import org.junit.jupiter.api.Assertions;
+import machineacafe.Machine;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import utilities.MachineBuilder;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MachineACafeTest {
-    @Test
-    @DisplayName("QUAND on met 40cts ALORS un café coule ET l'argent est encaissé")
-    public void Test_Cafe_Coule() {
+    @ParameterizedTest
+    @DisplayName("QUAND on met au moins 40cts ALORS un café coule ET l'argent est encaissé")
+    @ValueSource(doubles = {0.40, 0.41})
+    public void Test_Cafe_Coule(double sommeInsérée){
+        // ETANT DONNE une machine
+        Machine machine = MachineBuilder.Default();
+        int nombreCaféInitiaux = machine.GetNombreCafésServis();
+        double argentEncaisséInitial = machine.GetArgentEncaissé();
 
-        //ETANT DONNE  une machine
-        Machine machine = new Machine();
-        int nombreCafeInitiaux = machine.GetNombreCafesServis();
-        double argentEncaisseInitial = machine.GetArgentEncaisse();
-        double sommeInseree = 0.40;
+        // QUAND on met 40cts
+        machine.Insérer(sommeInsérée);
 
-        //QUAND on met 40cts
-        machine.Inserer(sommeInseree);
+        // ALORS un café coule
+        int nombreCafésFinaux = machine.GetNombreCafésServis();
+        assertEquals(nombreCaféInitiaux + 1, nombreCafésFinaux);
 
-        //ALORS un café coule
-        int nombreCafesFinaux = machine.GetNombreCafesServis();
-        Assertions.assertEquals(nombreCafeInitiaux +1, nombreCafesFinaux);
-
-        //ET l'argent est encaissé
-        double argentEncaisseFinal = machine.GetArgentEncaisse();
-        Assertions.assertEquals(argentEncaisseInitial + sommeInseree, argentEncaisseFinal);
+        // ET l'argent est encaissé
+        double argentEncaisséFinal = machine.GetArgentEncaissé();
+        assertEquals(argentEncaisséInitial + sommeInsérée, argentEncaisséFinal);
     }
 
     @Test
     @DisplayName("QUAND on met moins de 40cts ALORS aucun café ne coule ET l'argent est rendu")
-    public void Test_Pas_Assez_Argent() {
-
-        //ETANT DONNE  une machine
-        Machine machine = new Machine();
-        int nombreCafeInitiaux = machine.GetNombreCafesServis();
-        double argentEncaisseInitial = machine.GetArgentEncaisse();
+    public void Test_Pas_Assez_Argent(){
+        // ETANT DONNE une machine
+        Machine machine = MachineBuilder.Default();
+        int nombreCaféInitiaux = machine.GetNombreCafésServis();
+        double argentEncaisséInitial = machine.GetArgentEncaissé();
         double sommeInsuffisante = 0.39;
 
-        //QUAND on met moins de 40cts
-        machine.Inserer(sommeInsuffisante);
+        // QUAND on met moins de 40cts
+        machine.Insérer(sommeInsuffisante);
 
-        //ALORS aucun café ne coule
-        int nombreCafesFinaux = machine.GetNombreCafesServis();
-        Assertions.assertEquals(nombreCafeInitiaux, nombreCafesFinaux);
+        // ALORS aucun café ne coule
+        int nombreCafésFinaux = machine.GetNombreCafésServis();
+        assertEquals(nombreCaféInitiaux, nombreCafésFinaux);
 
-        //ET l'argent est rendu
-        double argentEncaisseFinal = machine.GetArgentEncaisse();
-        Assertions.assertEquals(argentEncaisseInitial, argentEncaisseFinal);
-    }
-
-    @Test
-    @DisplayName("QUAND on met plus de 40cts ALORS le café coule ET l'argent est encaissé")
-    public void Test_Somme_Inseree_Superieur_Au_Prix() {
-
-        //ETANT DONNE  une machine
-        Machine machine = new Machine();
-        int nombreCafeInitiaux = machine.GetNombreCafesServis();
-        double argentEncaisseInitial = machine.GetArgentEncaisse();
-        double sommeInseree = 0.41;
-
-        //QUAND on met plus de 40cts
-        machine.Inserer(sommeInseree);
-
-        //ALORS un café coule
-        int nombreCafesFinaux = machine.GetNombreCafesServis();
-        assertEquals(nombreCafeInitiaux +1, nombreCafesFinaux);
-
-        //ET l'argent est encaissé
-        double argentEncaisseFinal = machine.GetArgentEncaisse();
-        assertEquals(argentEncaisseInitial + sommeInseree, argentEncaisseFinal);
+        // ET l'argent est rendu
+        double argentEncaisséFinal = machine.GetArgentEncaissé();
+        assertEquals(argentEncaisséInitial, argentEncaisséFinal);
     }
 }
